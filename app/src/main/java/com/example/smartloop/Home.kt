da2c6b380e247617629b4d493546a8fed9c4d151
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-04 22:24:56 +0500

    c

--- app/src/main/java/com/example/smartloop/Home.kt
+++ app/src/main/java/com/example/smartloop/Home.kt
@@ -2,7 +2,12 @@ package com.example.smartloop
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.smartloop.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
@@ -13,8 +18,19 @@ class Home : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
         binding.toolbar.title = "Smart"
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
+            R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+//        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
 
 
     }
+
+
 }
