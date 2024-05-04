3faca0a3c3b67fccfd5195515c732b1d1c81c66b
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-04 10:57:51 +0500

    Add home  activity

--- app/src/main/java/com/example/smartloop/Home.kt
+++ app/src/main/java/com/example/smartloop/Home.kt
@@ -1,20 +1,21 @@
 package com.example.smartloop
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.smartloop.databinding.ActivityHomeBinding
+import com.example.smartloop.databinding.ActivityLoginBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
