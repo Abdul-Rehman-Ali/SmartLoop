7d2b0ac473c433dff7679b7e8da6411d8665ad66
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-04 11:48:56 +0500

    Add bottom Navbar

--- app/src/main/java/com/example/smartloop/Home.kt
+++ app/src/main/java/com/example/smartloop/Home.kt
@@ -1,13 +1,8 @@
 package com.example.smartloop
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.smartloop.databinding.ActivityHomeBinding
-import com.example.smartloop.databinding.ActivityLoginBinding
 
 class Home : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
