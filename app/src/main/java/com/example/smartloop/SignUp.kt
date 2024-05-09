b22d89b7228e5a063cf96aa0e33155c1a82eae08
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-09 23:10:31 +0500

    c

--- app/src/main/java/com/example/smartloop/SignUp.kt
+++ app/src/main/java/com/example/smartloop/SignUp.kt
@@ -1,20 +1,72 @@
 package com.example.smartloop
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.smartloop.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUp : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        binding.btnSignUp.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if(checkAllField()){
+                auth.createUserWithEmailAndPassword(email,password).addOnCanceledListener {
+
+                  Toast.makeText(this,"Successfully Account Created",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+    }
+
+    private fun checkAllField(): Boolean {
+        val email = binding.etEmail.text.toString()
+        if(binding.etUsername.text.toString() == ""){
+            binding.etUsername.error = "This is required field"
+            return false
+        }
+        if(binding.etEmail.text.toString() == ""){
+            binding.etEmail.error = "This is required field"
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.error = "Check email format"
+            return false
+        }
+        if (binding.etPhone.text.toString() == ""){
+            binding.etPhone.error = "This is required field"
+            return false
+        }
+        if (binding.etPassword.text.toString() == ""){
+            binding.etPassword.error = "This is required field"
+            return false
+        }
+        if (binding.etPassword.length() < 8){
+            binding.etPassword.error = "Password should be 8  characters"
+            return false
+        }
+        if (binding.etConfirmPassword.text.toString() == ""){
+            binding.etConfirmPassword.error = "This is required field"
+            return false
+        }
+        if (binding.etPassword.text.toString() != binding.etConfirmPassword.text.toString()){
+            binding.etPassword.error = "Password do not match"
+            return false
+        }
+
+        return true
     }
 }
