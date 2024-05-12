07c0a397649badbb3c32ea9778260fd984470347
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-12 21:43:30 +0500

    c

--- app/src/main/java/com/example/smartloop/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/smartloop/fragments/ProfileFragment.kt
@@ -23,7 +23,7 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment using view binding
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
