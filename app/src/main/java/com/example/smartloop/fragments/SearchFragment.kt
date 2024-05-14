6ae54be8e4760ae222890ccf62883e10ad5c2ed9
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-14 23:57:51 +0500

    c

--- app/src/main/java/com/example/smartloop/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/smartloop/fragments/SearchFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.smartloop.R
+import com.example.smartloop.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
-
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // Clean up reference to binding to avoid memory leaks
+    }
 }
