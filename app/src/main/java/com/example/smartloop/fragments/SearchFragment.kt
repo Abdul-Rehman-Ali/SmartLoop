e631f2b171724243476839f82f84c956a0320e0c
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-15 00:21:17 +0500

    c

--- app/src/main/java/com/example/smartloop/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/smartloop/fragments/SearchFragment.kt
@@ -1,5 +1,7 @@
 package com.example.smartloop.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,6 +19,32 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.phoneCall1.setOnClickListener {
+            val number = "+923181646340"
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.setData(Uri.parse("tel:$number"))
+            startActivity(intent)
+        }
+
+        binding.phoneCall2.setOnClickListener {
+            val number = "+923181646340"
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.setData(Uri.parse("tel:$number"))
+            startActivity(intent)
+        }
+
+        binding.phoneCall3.setOnClickListener {
+            val number = "+923181646340"
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.setData(Uri.parse("tel:$number"))
+            startActivity(intent)
+        }
+
     }
 
     override fun onDestroyView() {
