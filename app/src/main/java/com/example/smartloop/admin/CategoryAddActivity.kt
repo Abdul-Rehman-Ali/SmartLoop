8328c2cfffab400a606d00bf633b242461e2e29b
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-26 21:39:56 +0500

    c

---
+++ app/src/main/java/com/example/smartloop/admin/CategoryAddActivity.kt
@@ -0,0 +1,21 @@
+package com.example.smartloop.admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.smartloop.R
+
+class CategoryAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_category_add)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
