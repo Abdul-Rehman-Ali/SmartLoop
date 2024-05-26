ef817cf8d60a0a4fe61dd4ba3d0b101a08815a5c
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-27 00:08:40 +0500

    c

--- app/src/main/java/com/example/smartloop/admin/ModelCategorey.kt
+++ app/src/main/java/com/example/smartloop/admin/ModelCategorey.kt
@@ -1,4 +1,21 @@
 package com.example.smartloop.admin
 
+import com.google.firebase.Timestamp
+
 class ModelCategorey {
+
+    var id: String = ""
+    var category: String = ""
+    var timestamp: Long = 0
+    var uid : String = ""
+
+    constructor()
+
+    constructor(id:String, category: String, timestamp: Long, uInt: String){
+        this.id = id
+        this.category = category
+        this.timestamp = timestamp
+        this.uid = uid
+
+    }
 }
