3421517d45ce27551f55317f08ce7e74319cc87f
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-05-27 15:00:28 +0500

    c

--- app/src/main/java/com/example/smartloop/admin/DashboardAdmin.kt
+++ app/src/main/java/com/example/smartloop/admin/DashboardAdmin.kt
@@ -33,7 +33,7 @@ class DashboardAdmin : AppCompatActivity() {
         // search
         binding.etSearch.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+
             }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 try {
@@ -43,7 +43,7 @@ class DashboardAdmin : AppCompatActivity() {
                 }
             }
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
+
             }
 
         })
@@ -70,7 +70,7 @@ class DashboardAdmin : AppCompatActivity() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+
             }
         })
 
