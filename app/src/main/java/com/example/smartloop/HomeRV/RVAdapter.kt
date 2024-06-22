5610803bebe165101e893e29b3b91ae563d387c4
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-06-22 22:36:06 +0500

    c

--- app/src/main/java/com/example/smartloop/HomeRV/RVAdapter.kt
+++ app/src/main/java/com/example/smartloop/HomeRV/RVAdapter.kt
@@ -1,18 +1,19 @@
 package com.example.smartloop.HomeRV
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.smartloop.R
+import com.example.smartloop.admin.DashboardUserActivity
 
-class RVAdapter(val list: ArrayList<RVModel>, val images: Context) : Adapter<OurVH>() {
+class RVAdapter(private val list: ArrayList<RVModel>, private val context: Context) : RecyclerView.Adapter<OurVH>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OurVH {
-
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.course_sample_rv, parent, false)
         return OurVH(view)
@@ -23,13 +24,21 @@ class RVAdapter(val list: ArrayList<RVModel>, val images: Context) : Adapter<Our
     }
 
     override fun onBindViewHolder(holder: OurVH, position: Int) {
-        holder.img.setImageResource(list.get(position).image)
-        holder.tv.text = list.get(position).tv
+        val item = list[position]
+        holder.img.setImageResource(item.image)
+//        holder.tv.text = item.title
+
+        // Handle click event for the first card view
+        holder.itemView.setOnClickListener {
+           // Check if it's the first item
+                val intent = Intent(context, DashboardUserActivity::class.java)
+                context.startActivity(intent)
+
+        }
     }
 }
 
 class OurVH(view: View) : RecyclerView.ViewHolder(view) {
-    val img = view.findViewById<ImageView>(R.id.img)
-    val tv = view.findViewById<TextView>(R.id.tv_course)
-
+    val img: ImageView = view.findViewById(R.id.img)
+    val tv: TextView = view.findViewById(R.id.tv_course)
 }
--- app/src/main/java/com/example/smartloop/PdfDetailActivity.kt
+++ app/src/main/java/com/example/smartloop/PdfDetailActivity.kt
@@ -1,22 +1,42 @@
 package com.example.smartloop
 
+import android.Manifest
+import android.app.ProgressDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.provider.SyncStateContract.Constants
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.smartloop.HomeRV.Constant
 import com.example.smartloop.admin.MyApplication
 import com.example.smartloop.databinding.ActivityPdfDetailBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import java.io.FileOutputStream
 
 class PdfDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPdfDetailBinding
+    private companion object {
+        const val  TAG = "Book_DETAIL_TAG"
+    }
+
     private var bookId = ""
+    private var bookTitle = ""
+    private var bookUrl = ""
+
+    private lateinit var progressDialog : ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +48,10 @@ class PdfDetailActivity : AppCompatActivity() {
 
         bookId = intent.getStringExtra("bookId")!!
 
+        progressDialog =  ProgressDialog(this)
+        progressDialog.setTitle("Please wait...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
         MyApplication.incrementBookViewCount(bookId)
 
         loadBookDetail()
@@ -37,6 +61,106 @@ class PdfDetailActivity : AppCompatActivity() {
             intent.putExtra("bookId", bookId)
             startActivity(intent)
         }
+        binding.btnDownloadBook.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                Log.d(TAG, "onCreate: Storage Permission is already granted")
+                downloadBook()
+            }
+            else {
+                Log.d(TAG, "onCreate: Storage Permission was not granted, Lets request it")
+                requestStoragePermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    private val requestStoragePermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted : Boolean ->
+        if  (isGranted){
+            Log.d(TAG, "onCreate: Storage Permission is granted")
+        } else {
+            Log.d(TAG, "onCreate: Storage Permission is denied.")
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private  fun downloadBook(){
+
+        Log.d(TAG, "downloadBook: Downloading Book")
+        progressDialog.setMessage("Downloading Book")
+        progressDialog.show()
+
+        val storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(bookUrl)
+        storageRef.getBytes(com.example.smartloop.admin.Constants.Max_Byte_PDF)
+            .addOnSuccessListener {bytes->
+                Log.d(TAG, "downloadBook: Book Downloaded...")
+                saveToDownloadFolder(bytes)
+            }
+            .addOnFailureListener {e->
+                progressDialog.dismiss()
+                Log.d(TAG, "downloadBook: Failed to download book due to ${e.message} ")
+                Toast.makeText(this, "Failed to download book due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveToDownloadFolder(bytes: ByteArray?) {
+        Log.d(TAG, "saveToDownloadFolder: Saving Downloaded Book")
+        val  nameWithExtension = "$bookTitle.pdf"
+
+        try {
+            val downloadFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            downloadFolder.mkdirs()
+
+            val filePath = downloadFolder.path + "/" + nameWithExtension
+
+            val out = FileOutputStream(filePath)
+            out.write(bytes)
+            out.close()
+
+            Toast.makeText(this, "Saved to downloads folder", Toast.LENGTH_SHORT).show()
+            progressDialog.dismiss()
+            incrementDownloadCount()
+
+        }
+        catch (e: Exception){
+
+        }
+    }
+
+    private fun incrementDownloadCount() {
+        Log.d(TAG, "incrementDownloadCount: ")
+        val ref = FirebaseDatabase.getInstance().getReference("Book")
+        ref.child(bookId)
+            .addListenerForSingleValueEvent(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val downloadCount = "${snapshot.child("downloadCount").value}"
+                    Log.d(TAG, "onDataChange: Current Download Count : $downloadCount")
+
+                    if (downloadCount == "" || downloadCount == "null"){
+                        downloadCount == "0"
+                    }
+
+                    val newDownloadCount : Long = downloadCount.toLong() + 1
+                    Log.d(TAG, "onDataChange: New Current Download Count : $newDownloadCount")
+
+                    val hashMap: HashMap<String, Any> = HashMap()
+                    hashMap["downloadCount"] = newDownloadCount
+
+                    val dbRef = FirebaseDatabase.getInstance().getReference("Book")
+                    dbRef.child(bookId)
+                        .updateChildren(hashMap)
+                        .addOnSuccessListener {
+                            Log.d(TAG, "onDataChange: Downloads  Count Incremented")
+                        }
+                        .addOnFailureListener {e->
+                            Log.d(TAG, "onDataChange: Failed to increment due to ${e.message}")
+                        }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
     }
 
     private fun loadBookDetail() {
@@ -48,18 +172,18 @@ class PdfDetailActivity : AppCompatActivity() {
                     val description = snapshot.child("description").getValue(String::class.java) ?: ""
                     val downloadCount = snapshot.child("downloadCount").getValue(Long::class.java) ?: 0L
                     val timestamp = snapshot.child("timestamp").getValue(Long::class.java) ?: 0L
-                    val title = snapshot.child("title").getValue(String::class.java) ?: ""
+                    bookTitle = snapshot.child("title").getValue(String::class.java) ?: ""
                     val uid = snapshot.child("uid").getValue(String::class.java) ?: ""
-                    val url = snapshot.child("url").getValue(String::class.java) ?: ""
+                    bookUrl = snapshot.child("url").getValue(String::class.java) ?: ""
                     val viewCount = snapshot.child("viewCount").getValue(Long::class.java) ?: 0L
 
                     val date = MyApplication.formatTimeStamp(timestamp)
 
                     MyApplication.loadCategory(categoryId, binding.tvCategory)
-                    MyApplication.loadPdfFromUrlSinglePage(url, title, binding.pdfview, binding.progressBar, binding.tvPages)
-                    MyApplication.loadPdfSize(url, title, binding.tvSize)
+                    MyApplication.loadPdfFromUrlSinglePage(bookUrl, bookTitle, binding.pdfview, binding.progressBar, binding.tvPages)
+                    MyApplication.loadPdfSize(bookUrl, bookTitle, binding.tvSize)
 
-                    binding.tvTitle.text = title
+                    binding.tvTitle.text = bookTitle
                     binding.tvDescription.text = description
                     binding.tvViews.text = viewCount.toString()
                     binding.tvDownloads.text = downloadCount.toString()
